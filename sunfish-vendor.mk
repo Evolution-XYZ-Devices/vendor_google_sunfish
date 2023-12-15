57fe5d2846bc618a93e0d37449326cfc14f83ca7
lahaina <nattolecats@gmail.com>
2023-12-15 22:02:10 +0900

    sunfish: Avoid AiAi package conflicts

--- sunfish-vendor.mk
+++ sunfish-vendor.mk
@@ -76,8 +76,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel4 \
     DiagMon \
     EuiccGoogle \
     HardwareInfo \
